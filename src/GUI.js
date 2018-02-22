9bfd41cc87c1ef1f153d915314b6de81d7d293c2
Chris <Phogue-@live.com>
2018-02-22 09:19:11 +0100

    fixed mappeopsætning
    fixed måske

--- src/GUI.js
+++ src/GUI.js
@@ -6,8 +6,15 @@ function updateDice(){
     var links = ["one.png", "two.png", "three.png", "four.png", "five.png", "six.png"];
     var dieField = $(".die");
     for(var i = 0; i < dice.length; i++){
-        if(holds[i] === false)  dieField[i].innerHTML = ("<img src=\"../images/" + links[dice[i]-1] + "\">");
+        if(holds[i] === false)  dieField[i].innerHTML = ("<img src=\"images/" + links[dice[i]-1] + "\">");
     }
+}
+
+function clear(){
+    var fields = $(".results");
+    fields.foreach(function(element){
+        element.text = "";
+    });
 }
 
 //funktioner der kører på page load
@@ -19,7 +26,12 @@ $(function(){
       turn++;
       updateDice();
 
-  })
+  });
+  //save-function når man klikker på et result felt
+  $(".clear").on("click", function() {
+    clear();
+
+  });
 
 
     //hold-funktionalitet når man clicker på terninger
@@ -31,7 +43,5 @@ $(function(){
             $(this).removeClass("held");
             holds[Number($(this).attr("id")-1)] = false;
         }
-
-
     })
 });
